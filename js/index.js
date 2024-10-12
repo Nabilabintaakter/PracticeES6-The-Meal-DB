c2c6e713fc1ab0a2f2654673e1f124dafcbcd067
Nabilabintaakter <nabilabintaakter@gmail.com>
2024-10-12 21:08:44 +0600

    edited the homepage layout

--- js/index.js
+++ js/index.js
@@ -1,31 +1,39 @@
 // fetch data from API-----------------------------------------------------
-
+// load category names
 const loadAllCategory = ()=>{
     fetch(`https://www.themealdb.com/api/json/v1/1/categories.php`)
     .then(res => res.json())
     .then(data => displayAllCategory(data.categories))
 }
+// load category wise data
+const loadCategoryDetails = (categoryName)=>{
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${categoryName}`)
+    .then(res => res.json())
+    .then(data => displayCategoryDetails(data.meals))
+}
 
-
-// display all categories
+// display all categories by default
 const displayAllCategory = (categories)=>{
     const categoryContainer = document.getElementById("category-container");
     categories.forEach(category => {
         const div = document.createElement('div');
         div.innerHTML = `
-        <div id="category-${category.idCategory}" class="h-auto flex items-center gap-5 border-[1px] border-text-primary border-opacity-10 rounded-lg p-4">
-                    <div class="w-1/2 h-full  flex justify-center items-center"><img class="w-full h-full object-cover" src="${category.strCategoryThumb}" alt=""></div>
-                    <div class="w-1/2">
+        <div id="category-${category.idCategory}" class="h-auto flex flex-col sm:flex-row items-center gap-5  rounded-lg p-4 shadow-xl">
+                    <div class="w-full sm:w-1/2 h-full  flex justify-center items-center"><img class="w-full h-full object-cover" src="${category.strCategoryThumb}" alt=""></div>
+                    <div class="w-full sm:w-1/2">
                         <h1 class="text-text-secondary text-xl sm:text-2xl font-bold mb-2">${category.strCategory}</h1>
-                        <p class="text-text-tertiary text-sm sm:text-lg mb-3 h-28 overflow-y-scroll" style="-ms-overflow-style: none; scrollbar-width: none;">${category.strCategoryDescription}</p>
-                        <a class="underline text-btn-primary text-sm lg:text-lg font-semibold" href="">View details</a>
+                        <p class="text-text-tertiary text-base sm:text-lg mb-4 h-[96px] sm:h-[105px] md:h-28 overflow-y-scroll" style="-ms-overflow-style: none; scrollbar-width: none;">${category.strCategoryDescription}</p>
+                        <p onclick="loadCategoryDetails('${category.strCategory}')" class="underline text-btn-primary text-base lg:text-lg font-semibold cursor-pointer">View details</p>
                     </div>
                 </div>
         `;
         categoryContainer.append(div);
     });
 }
+// display all categories 
+const displayCategoryDetails = ()=>{
 
+}
 
 
 
