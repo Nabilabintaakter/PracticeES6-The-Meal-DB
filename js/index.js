85e25b6a51b5a6be611f25f605035d4459595f5a
Nabilabintaakter <nabilabintaakter@gmail.com>
2024-10-12 22:59:10 +0600

    fetched and displayed category wise meal

--- js/index.js
+++ js/index.js
@@ -15,6 +15,9 @@ const loadCategoryDetails = (categoryName)=>{
 // display all categories by default
 const displayAllCategory = (categories)=>{
     const categoryContainer = document.getElementById("category-container");
+
+    document.getElementById('category-foods-container').classList.add('hidden')
+    document.getElementById('category-foods-container').classList.remove('grid')
     categories.forEach(category => {
         const div = document.createElement('div');
         div.innerHTML = `
@@ -31,11 +34,50 @@ const displayAllCategory = (categories)=>{
     });
 }
 // display all categories 
-const displayCategoryDetails = ()=>{
-
+const displayCategoryDetails = (meals)=>{
+    document.getElementById('category-container').innerHTML = '';
+    const categoryFoodsContainer = document.getElementById('category-foods-container');
+    categoryFoodsContainer.innerHTML = '';
+    document.getElementById('category-foods-container').classList.remove('hidden')
+    document.getElementById('category-foods-container').classList.add('grid')
+    meals.forEach(meal=>{
+        console.log(meal);
+        const div = document.createElement('div');
+        div.innerHTML=`
+        <div class="card bg-base-100 shadow-xl rounded-xl card-side  flex-col ">
+            <div>
+            <img class="rounded-xl w-full"
+                src="${meal.strMealThumb}"
+                alt="Shoes" />
+            </div>
+            <div class="card-body flex flex-col space-y-4">
+                <h2 class="card-title text-2xl">${meal.strMeal}</h2>
+                <button id="btn-view-details" onclick="loadMealDetails(${meal.idMeal})"
+                    class="btn text-text-primary btn-md md:btn-lg bg-btn-primary border-none font-bold md:font-extrabold w-fit">View Details
+                </button>
+            </div>
+</div>
+        `;
+        categoryFoodsContainer.append(div);
+    })
 }
 
+// {
+//     "strMeal": "Beef and Mustard Pie",
+//     "strMealThumb": "https://www.themealdb.com/images/media/meals/sytuqu1511553755.jpg",
+//     "idMeal": "52874"
+// }
 
+// {/* <div class="card card-side bg-base-100 shadow-xl flex-col">
+//                     <div>
+//                       <img class="w-auto rounded-xl" src="https://www.themealdb.com/images/media/meals/sytuqu1511553755.jpg" alt="food">
+//                     </div>
+//                     <div class="card-body items-start">
+//                       <h2 class="text-2xl card-title pb-5">Beef and Mustard ...</h2>
+//                       <p class="text-gray-400"></p>
+//                       <button onclick="openModal('52874')" class="btn btn-warning">View Details</button>
+//                     </div>
+//                 </div> */}
 
 // {
 //     "idCategory": "1",
